5ea2bb44bb381d4c56b8cf4b2fe4eea51a0704d2
santi <santiago.segundo@fing.edu.uy>
2022-08-24 01:26:44 -0300

    add docker compose installer for ubuntu

---
+++ docker_compose_ubuntu_installer.sh
@@ -0,0 +1,4 @@
+VERSION=$(curl --silent https://api.github.com/repos/docker/compose/releases/latest | grep -Po '"tag_name": "\K.*\d')
+DESTINATION=/usr/local/bin/docker-compose
+sudo curl -L https://github.com/docker/compose/releases/download/${VERSION}/docker-compose-$(uname -s)-$(uname -m) -o $DESTINATION
+sudo chmod 755 $DESTINATION
