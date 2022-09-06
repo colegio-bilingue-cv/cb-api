7c0fdc160525194c4dadbdab459cc4fd7c08cc2a
Maria <maria.usuca@gmail.com>
2022-09-05 22:52:57 -0300

    Create model student

---
+++ app/controllers/api/students_controller.rb
@@ -0,0 +1,12 @@
+class Api::StudentController < BaseController
+  def create
+    student = Student.create(student_params)
+    render json: student, status: :created #201
+
+  end
+
+    private
+    def student_params
+      params.require(:student).permit(:ci, :surname, :second_surname, :first_name, :middle_name, :birthplace, :birthdate, :nacionality, :first_language, :address, :neighborhood, :medical_assurance, :emergency, :vaccine_name, :vaccine_expiry)
+    end
+end
---
+++ db/migrate/20220906001245_create_students.rb
@@ -0,0 +1,31 @@
+class CreateStudents < ActiveRecord::Migration[7.0]
+  def change
+    create_table :students do |t|
+      t.string :ci
+      t.string :surname
+      t.string :second_surname
+      t.string :first_name
+      t.string :middle_name
+      t.string :group
+      t.string :sub_group
+      t.boolean :morning?
+      t.string :tuition
+      t.integer :reference_number
+      t.string :birthplace
+      t.datetime :birthdate
+      t.string :nacionality
+      t.string :first_language
+      t.string :office
+      t.string :state
+      t.string :address
+      t.string :neighborhood
+      t.string :medical_assurance
+      t.string :emergency
+      t.string :vaccine_name
+      t.datetime :vaccine_expiry
+
+
+      t.timestamps
+    end
+  end
+end
