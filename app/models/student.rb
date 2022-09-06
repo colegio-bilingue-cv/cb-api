f2d1eb772419e1fe702c50dca07a6bcd177eac9a
Maria <maria.usuca@gmail.com>
2022-09-06 19:05:04 -0300

    Validaciones

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,2 +1,8 @@
 class Student < ApplicationRecord
+  validates :ci, presence: true, length: {minimum: 8}, uniqueness: true
+  validates :surname, :first_name, presence: true
+  #validates :second_surname, :middle_name, 
+  validates :birthplace, :birthdate, :nacionality, :first_language, presence: true
+  validates :address, :neighborhood, presence: true
+  validares :medical_assurance, :emergency, :vaccine_name, :vaccine_expiry, presence: true
 end
