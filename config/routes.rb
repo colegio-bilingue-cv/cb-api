3929cd306cd283f09fe2e96d38666c0246d41f58
Maria <maria.usuca@gmail.com>
2022-09-08 21:19:10 -0300

    modelando mer
    Co-authored-by: Chuardo <Chuardo@users.noreply.github.com>

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   namespace :api do 
-    resources :students, only: [:create]
+    resources :students, only: [:create] do
+      resources :family_members, only: [:create]
+    end
+    
   end
 end
