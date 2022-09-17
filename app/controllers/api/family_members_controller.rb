73d6cc6782356f4e629218f8ae1f07aa345d3232
Chuardo <46316828+Chuardo@users.noreply.github.com>
2022-09-17 15:36:21 -0300

    Added Intex to Family Members (#13)

--- app/controllers/api/family_members_controller.rb
+++ app/controllers/api/family_members_controller.rb
@@ -2,7 +2,7 @@ class Api::FamilyMembersController < Api::BaseController
 
   def create
     student = Student.find(params[:student_id])
-    student.family_members.create!(family_member_params)
+    family_member = student.family_members.create!(family_member_params)
 
     render json: FamilyMemberSerializer.new.serialize_to_json(family_member), status: :created
   rescue ActiveRecord::RecordNotFound
@@ -11,9 +11,15 @@ class Api::FamilyMembersController < Api::BaseController
     render json: {}, status: :unprocessable_entity
   end
 
+  def index
+    family_members = FamilyMember.all
+    render json: Panko::ArraySerializer.new(family_members, each_serializer: FamilyMemberSerializer).to_json, status: :ok
+  end
+
+
   private
 
-  def family_members_params
+  def family_member_params
     params.require(:family_member).permit(:ci, :role, :full_name, :birthdate, :birthplace, :nationality,
       :first_language, :marital_status, :cellphone, :email,
       :address, :neighborhood, :education_level, :occupation, :workplace,
