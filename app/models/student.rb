39f93a16f19982726104fc374d84095d48f5a7b6
Chuardo <egastelu@gmail.com>
2022-09-25 17:00:57 -0300

    has_and_belongs_to_many to has_many

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,6 +1,6 @@
 class Student < ApplicationRecord
   has_and_belongs_to_many :family_members
-  has_and_belongs_to_many :type_scholarships
+  has_many :types, :agreements => :type_scholarships
   enum status: [:pending, :active, :inactive]
 
   validates :ci, presence: true, length: { minimum: 8 }, uniqueness: true
