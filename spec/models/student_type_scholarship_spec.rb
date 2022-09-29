be39c5eeca4512f62c1f40655a08c05096c890f8
Chuardo <egastelu@gmail.com>
2022-09-28 23:06:26 -0300

    done

--- spec/models/student_type_scholarship_spec.rb
+++ spec/models/student_type_scholarship_spec.rb
@@ -2,9 +2,6 @@ require 'rails_helper'
 
 RSpec.describe TypeScholarship, type: :model do
   describe StudentTypeScholarship do
-    context 'validations' do
-      it { should validate_presence_of(:date)}
-    end
     context 'relations' do
       it { should belong_to (:student) }
       it { should belong_to (:type_scholarship) }
