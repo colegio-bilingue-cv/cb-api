063d2d2b1c61807b5e4e5d816c6883156fdaf284
Joaquin <joakomega97@gmail.com>
2022-10-21 17:42:26 -0300

    -Pequena correccion en controlador de ciclos

--- app/controllers/api/cicles_controller.rb
+++ app/controllers/api/cicles_controller.rb
@@ -1,8 +1,11 @@
 class Api::CiclesController < ApplicationController
   def index
     cicles = Cicle.all
+    
     response = Panko::Response.new(
-      cicles: Panko::ArraySerializer.new(cicles, each_serializer: CicleSerializer).to_json, status: :ok
+      groups: Panko::ArraySerializer.new(cicles, each_serializer: CicleSerializer)
     )
+    
+    render json: response, status: :ok
   end
 end
