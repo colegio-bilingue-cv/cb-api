be3da41fd061b8faf5ac1a99eeead9b9c813b204
Joaquin <joakomega97@gmail.com>
2022-10-21 16:48:57 -0300

    -Arrancando

--- config/routes.rb
+++ config/routes.rb
@@ -28,7 +28,6 @@ Rails.application.routes.draw do
     resources :student_payment_methods, only: [:create, :update]
     resources :cicles, only: [:index]
 
-<<<<<<< HEAD
     resources :groups, only: [:index]
     resources :grades, only: [:index, :show] do
       resources :groups, only: [:create, :update]
@@ -37,9 +36,5 @@ Rails.application.routes.draw do
     resource :me, only: [:show, :update], controller: :me do
       patch :password
     end
-=======
-
-    
->>>>>>> 3c9c930 (-Arrancando)
   end
 end
