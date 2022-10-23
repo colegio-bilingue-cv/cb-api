3d25269aa7a70da260f115680294bd2b0fba645d
MariaU <maria.usuca@gmail.com>
2022-10-23 17:21:29 -0300

    Update me - sin contrasenia ni rol

--- app/controllers/api/me_controller.rb
+++ app/controllers/api/me_controller.rb
@@ -1,7 +1,7 @@
 class Api::MeController < Api::BaseController
 
   def show
-    
+
     response = Panko::Response.create do |r|
       { me: r.serializer(current_user, UserSerializer) }
     end
@@ -9,4 +9,17 @@ class Api::MeController < Api::BaseController
     render json: response, status: :ok
   end
 
+  def update
+    current_user.update!(me_params)
+
+    response = Panko::Response.create do |r|
+      { user: r.serializer(current_user, UserSerializer) }
+    end
+
+    render json: response, status: :ok
+  end
+
+  def me_params
+    params.require(:user).permit(:birthdate, :name, :surname, :ci, :address)
+  end
 end
