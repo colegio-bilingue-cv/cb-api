bff25d025b2949f5d69a364499f21d08c12085cd
Chuardo <46316828+Chuardo@users.noreply.github.com>
2022-10-28 18:48:25 -0300

    BE-183 : Crear boletin intermedio para un alumno (#52)
    * BE-183 : Crear boletin intermedio para un alumno
    
    * fixes post PR

---
+++ app/controllers/api/intermediate_evaluation_controller.rb
@@ -0,0 +1,22 @@
+class Api::IntermediateEvaluationController < Api::BaseController
+  def create
+    raise ActiveRecord::RecordNotFound.new('', Group.to_s) unless Group.exists?(intermediate_evaluation_params[:group_id])
+    raise ActiveRecord::RecordNotFound.new('', Student.to_s) unless Student.exists?(params[:student_id])
+
+    intermediate_evaluation = IntermediateEvaluation.create!(intermediate_evaluation_params) do |intermediate_evaluation|
+      intermediate_evaluation.student_id = params[:student_id]
+    end
+
+    response = Panko::Response.create do |r|
+      { intermediate_evaluation: r.serializer(intermediate_evaluation, IntermediateEvaluationSerializer) }
+    end
+
+    render json: response, status: :created
+  end
+
+  private
+
+  def intermediate_evaluation_params
+    params.require(:intermediate_evaluation).permit(:group_id, :starting_month, :ending_month)
+  end
+end
--- app/models/intermediate_evaluation.rb
+++ app/models/intermediate_evaluation.rb
@@ -3,12 +3,15 @@ class IntermediateEvaluation < ApplicationRecord
   belongs_to :group
 
   validates :starting_month, :ending_month, presence: true
+
   validate :month_period
+
+  delegate :name, :year, to: :group, prefix: true
 
   private
 
   def month_period
-    if starting_month.month > ending_month.month
+    if starting_month&.month &.> ending_month&.month
       errors.add(:starting_month, I18n.t('intermediate_evalution.errors.starting_month'))
     end
   end
