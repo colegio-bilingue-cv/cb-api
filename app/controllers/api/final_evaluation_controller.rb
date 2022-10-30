7562e18b65ea4708f0da06c07ed6fb8f94e02e4b
Chuardo <46316828+Chuardo@users.noreply.github.com>
2022-10-30 10:29:26 -0300

    BE-196: Modificación de boletin Intermedio para un alumno (#56)
    Co-authored-by: Guillermo Chao <14169739+cumanachao@users.noreply.github.com>

--- app/controllers/api/final_evaluation_controller.rb
+++ app/controllers/api/final_evaluation_controller.rb
@@ -12,6 +12,21 @@ class Api::FinalEvaluationController < Api::BaseController
     render json: response, status: :created
   end
 
+  def update
+    raise ActiveRecord::RecordNotFound.new('', Student.to_s) unless Student.exists?(final_evaluation_params[:student_id])
+    raise ActiveRecord::RecordNotFound.new('', Group.to_s) unless Group.exists?(final_evaluation_params[:group_id])
+
+    final_evaluation = FinalEvaluation.find(params[:id])
+    final_evaluation.update!(final_evaluation_params)
+
+    response = Panko::Response.create do |r|
+      { final_evaluation: r.serializer(final_evaluation, FinalEvaluationSerializer) }
+    end
+
+    render json: response, status: :ok
+  end
+
+
   private
 
   def final_evaluation_params
