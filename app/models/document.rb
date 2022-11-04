505055648ea810adb064475ecfbb7388e996fd62
MariaU <maria.usuca@gmail.com>
2022-11-04 18:00:05 -0300

    fix

--- app/models/document.rb
+++ app/models/document.rb
@@ -2,7 +2,7 @@ class Document < ApplicationRecord
   belongs_to :user
   has_one_attached :certificate
 
-  enum explanation: [:project, :evaluation]
+  enum document_type: [:project, :evaluation]
 
   validates :document_type, presence: true
 
