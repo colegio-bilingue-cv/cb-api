6d16e2f9e9d790504d79e9012bcc32a76e028326
MariaU <maria.usuca@gmail.com>
2022-11-09 18:04:40 -0300

    Añadir ci

--- app/serializers/teacher_serializer.rb
+++ app/serializers/teacher_serializer.rb
@@ -1,5 +1,5 @@
 class TeacherSerializer < Panko::Serializer
-  attributes :id, :name, :surname
+  attributes :id, :ci, :name, :surname
 
   has_many :teacher_groups, each_serializer: GroupWithGradeTeachersPrincipalSerializer, name: :groups
 end
