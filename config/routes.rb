2297ab2116dd9efd57bece63f5dfa9dae8a38a19
Maria <54046076+Usuca@users.noreply.github.com>
2022-11-08 23:15:21 -0300

    BE-157: Baja de boletín final(#87)

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,7 @@ Rails.application.routes.draw do
     resources :students, except: [:destroy] do
       resources :family_members, only: [:create, :update]
       resources :comments, only: [:create, :update]
-      resources :final_evaluation, only: [:create, :update]
+      resources :final_evaluation, only: [:create, :update, :destroy]
       resources :intermediate_evaluation, only: [:create, :update]
 
       get :family_members
